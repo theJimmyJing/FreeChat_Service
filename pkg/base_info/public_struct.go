7d97bfba222c113f2aa689ad0e5ae4e9bf6f03e6
venusaulis <costonlola@proton.me>
2022-11-12 23:32:47 +0800

    refactor update user info, expend gender

--- pkg/base_info/public_struct.go
+++ pkg/base_info/public_struct.go
@@ -12,7 +12,7 @@ type ApiUserInfo struct {
 	Password    string `json:"password" binding:"omitempty,min=1,max=64" swaggo:"true,password,"`
 	Nickname    string `json:"nickname" binding:"omitempty,min=1,max=64" swaggo:"true,my id,19"`
 	FaceURL     string `json:"faceURL" binding:"omitempty,max=1024"`
-	Gender      int32  `json:"gender" binding:"omitempty,oneof=0 1 2"`
+	Gender      int32  `json:"gender" binding:"omitempty,oneof=-1 0 1 2 3"`
 	PhoneNumber string `json:"phoneNumber" binding:"omitempty,max=32"`
 	Birth       uint32 `json:"birth" binding:"omitempty"`
 	Ex          string `json:"ex" binding:"omitempty,max=1024"`
